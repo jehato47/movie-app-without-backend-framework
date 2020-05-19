b02015a65fba761a2921b02da648060560ae025d
jehat armanç deniz <45170719+jehato47@users.noreply.github.com>
2020-05-19 09:56:02 +0300

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -4,20 +4,33 @@ let search3 = "&include_adult=false"
 
 
 
-
 document.getElementById("search_form").addEventListener("submit", (e) => {
     target = document.getElementById("search").value
     url = search + target + search2 + "1" + search3
-    fetch(url)
-        .then(response => response.text())
-        .then(response => console.log(JSON.parse(response)))
+
+
     let cards = document.getElementById("cards")
 
     // This is because if you go to details it will clear secondary div
-    cards != undefined ?  cards.innerHTML = "": document.getElementById("secondary-div").innerHTML=""
-    cards != undefined ?  cards.innerHTML = "": document.getElementById("secondary-div").innerHTML+=cards
-
-    fulfill_cards(search + target + search2, "secondary-div")
+    
+        document.getElementById("secondary-div").innerHTML=""
+        document.getElementById("secondary-div").innerHTML+=`<div id="div-sort" class="mx-auto col-8 text-center ">
+        <h4 id="h4-sort" class="text-center">Sort by</h4>
+        <div>
+            <button id="btn-sort-1" onclick="populars_f()" class="btn-sort">most popular</button>
+            <button id="btn-sort-2" onclick="latest_f()" class="btn-sort">newest</button>
+            <button id="btn-sort-3" onclick="top_rated_f()" class="btn-sort">best rated</button>
+        </div>
+    </div>`
+        fulfill_cards(search + target + search2, "secondary-div")
 
     e.preventDefault()
 })
+
+
+
+
+
+
+
+
